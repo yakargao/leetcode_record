b550675d8666d1db1ab2e1b332cb28287ec153e0
CiachoG <35428353+CiachoG@users.noreply.github.com>
2020-05-05 10:13:33 +0800

    the same tree

--- 100_theSameTree.go
+++ 100_theSameTree.go
@@ -4,3 +4,24 @@
 * @Description：
  */
 package algorithm
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
---
+++ 101_isSymmetric.go
@@ -0,0 +1,27 @@
+/**
+* @Author: CiachoG
+* @Date: 2020/4/27 14:44
+* @Description：
+ */
+package algorithm
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	isMirror(root, root)
+}
+func isMirror(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+	return left.Val == right.Val && isMirror(left.Left, right.Right) && isMirror(left.Right, right.Left)
+}
